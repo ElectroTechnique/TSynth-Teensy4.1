124ea03f7c8faec833596478e92c154eb508f0a8
ElectroTechnique <50934180+ElectroTechnique@users.noreply.github.com>
2021-04-27 17:24:03 +0900

    Way hey, pitchbend's back! Quite easy too.

--- TSynth/VoiceGroup.h
+++ TSynth/VoiceGroup.h
@@ -778,7 +778,7 @@ class VoiceGroup {
     }
 
     void pitchBend(float amount) {
-        shared.pitchLfo.amplitude(amount);
+        shared.pitchBend.amplitude(amount);
     }
 
     void midiClockStart() {
