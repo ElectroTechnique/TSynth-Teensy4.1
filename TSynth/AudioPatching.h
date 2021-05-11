04745f18690b96d43799534bf7ec012344f21977
Will Winder <wwinder.unh@gmail.com>
2021-05-10 20:36:04 -0400

    Move global audio objects into a struct.

--- TSynth/AudioPatching.h
+++ TSynth/AudioPatching.h
@@ -1,17 +1,11 @@
 #ifndef TSYNTH_AUDIO_PATCHING_H
 #define TSYNTH_AUDIO_PATCHING_H
 
+#include <vector>
+#include "Constants.h"
+
 //waveformX      -->   waveformMixerX   -->   voiceMixer1-3   -->   voiceMixerM  --> volumeMixer
 //WAVEFORMLEVEL        oscA/BLevel             VELOCITY    VOICEMIXERLEVEL/UNISONVOICEMIXERLEVEL    volume
-
-// Make the visual audio design tool work by substituting the *TS locally changed classes by those it knows: 
-// but also ensure all redefined class names are undefined within this scope, if lingering defines exist, raise an error:
-#if defined(AudioSynthWaveform) || defined(AudioSynthWaveformModulated)
-# error "Either AudioSynthWaveform or AudioSynthWaveformModulated are already redefined!"
-#endif
-#define AudioSynthWaveform          AudioSynthWaveformTS
-#define AudioSynthWaveformModulated AudioSynthWaveformModulatedTS
-
 
 // Oscillator configurations.
 struct Patch {
@@ -27,9 +21,9 @@ struct Patch {
     AudioMixer4 oscModMixer_a;
     AudioMixer4 oscModMixer_b;
 
-    AudioSynthWaveform filterLfo_;
-    AudioSynthWaveformModulated waveformMod_a;
-    AudioSynthWaveformModulated waveformMod_b;
+    AudioSynthWaveformTS filterLfo_;
+    AudioSynthWaveformModulatedTS waveformMod_a;
+    AudioSynthWaveformModulatedTS waveformMod_b;
 
     AudioEffectDigitalCombine oscFX_;
 
@@ -89,11 +83,11 @@ struct Patch {
 
 struct PatchShared {
     AudioSynthWaveformDc pitchBend;
-    AudioSynthWaveform pitchLfo;
+    AudioSynthWaveformTS pitchLfo;
     AudioMixer4 pitchMixer;
-    AudioSynthWaveform pwmLfoA;
-    AudioSynthWaveform pwmLfoB;
-    AudioSynthWaveform filterLfo;
+    AudioSynthWaveformTS pwmLfoA;
+    AudioSynthWaveformTS pwmLfoB;
+    AudioSynthWaveformTS filterLfo;
     AudioSynthWaveformDc pwa;
     AudioSynthWaveformDc pwb;
     AudioMixer4 noiseMixer;
@@ -112,74 +106,86 @@ struct PatchShared {
     };
 };
 
+const uint8_t MAX_NO_TIMBER = 2;
+const uint8_t MAX_NO_VOICE = 12;
 
-// GUItool: begin automatically generated code
-AudioOutputUSB           usbAudio;       //xy=3197,1821
-AudioSynthWaveformDc     constant1Dc;    //xy=69,1781
-AudioSynthNoisePink      pink;           //xy=1462
-AudioSynthNoiseWhite     white;          //xy=1460
-AudioAnalyzePeak         peak;           //xy=2756,1817
-AudioMixer4              voiceMixer1;    //xy=2233,581
-AudioMixer4              voiceMixer2;    //xy=2240,1791
-AudioMixer4              voiceMixer3;    //xy=2237,2976
-AudioMixer4              voiceMixerM;    //xy=2431,1798
-AudioFilterStateVariable dcOffsetFilter; //xy=2591,1804
-AudioMixer4              volumeMixer;    //xy=2774,1756
-AudioEffectEnsemble      ensemble;      //xy=1800,600
-Oscilloscope             scope;
-AudioMixer4              effectMixerR;   //xy=2984,1823
-AudioMixer4              effectMixerL;   //xy=2985,1728
-AudioOutputI2S           i2s;            //xy=3190,1737
+struct Global {
+    AudioOutputUSB           usbAudio;
+    AudioSynthWaveformDc     constant1Dc;
+    AudioSynthNoisePink      pink;
+    AudioSynthNoiseWhite     white;
+    AudioAnalyzePeak         peak;
+    AudioMixer4              voiceMixer[3];
+    AudioMixer4              voiceMixerM;
+    AudioFilterStateVariable dcOffsetFilter;
+    AudioMixer4              volumeMixer;
+    AudioEffectEnsemble      ensemble;
+    Oscilloscope             scope;
+    AudioMixer4              effectMixerR;
+    AudioMixer4              effectMixerL;
+    AudioOutputI2S           i2s;
 
-PatchShared SharedAudio[2];
-Patch Oscillators[12];
+    PatchShared SharedAudio[MAX_NO_TIMBER];
+    Patch Oscillators[MAX_NO_VOICE];
 
-// 
-AudioConnection          patchCord4(constant1Dc, Oscillators[0].filterEnvelope_);
-AudioConnection          patchCord1(constant1Dc, Oscillators[1].filterEnvelope_);
-AudioConnection          patchCord2(constant1Dc, Oscillators[2].filterEnvelope_);
-AudioConnection          patchCord3(constant1Dc, Oscillators[3].filterEnvelope_);
-AudioConnection          patchCord199(constant1Dc, Oscillators[4].filterEnvelope_);
-AudioConnection          patchCord200(constant1Dc, Oscillators[5].filterEnvelope_);
-AudioConnection          patchCord228(constant1Dc, Oscillators[6].filterEnvelope_);
-AudioConnection          patchCord229(constant1Dc, Oscillators[7].filterEnvelope_);
-AudioConnection          patchCord230(constant1Dc, Oscillators[8].filterEnvelope_);
-AudioConnection          patchCord231(constant1Dc, Oscillators[9].filterEnvelope_);
-AudioConnection          patchCord232(constant1Dc, Oscillators[10].filterEnvelope_);
-AudioConnection          patchCord233(constant1Dc, Oscillators[11].filterEnvelope_);
+    AudioControlSGTL5000     sgtl5000_1;     //xy=2353,505
 
-// Global Shared Audio Connections
-AudioConnection          patchCord452(pink, 0, SharedAudio[0].noiseMixer, 0);
-AudioConnection          patchCord453(pink, 0, SharedAudio[1].noiseMixer, 0);
+    std::vector<AudioConnection*> connections;
+    Global(float mixerLevel) {
+        for (int i = 0; i < MAX_NO_VOICE; i++) {
+            connections.push_back(new AudioConnection{constant1Dc, Oscillators[i].filterEnvelope_});
+        }
+        for (int i = 0; i < MAX_NO_TIMBER; i++) {
+            connections.push_back(new AudioConnection{pink, 0, SharedAudio[i].noiseMixer, 0});
+            connections.push_back(new AudioConnection{white, 0, SharedAudio[i].noiseMixer, 1});
 
-AudioConnection          patchCord137(white, 0, SharedAudio[0].noiseMixer, 1);
-AudioConnection          patchCord138(white, 0, SharedAudio[1].noiseMixer, 1);
+            uint8_t mixerIdx = 0;
+            if (i > 0) mixerIdx = i / 12;
+            uint8_t chanIdx = i % 4;
+            connections.push_back(new AudioConnection{SharedAudio[i].voiceMixerM, 0, voiceMixer[mixerIdx], chanIdx});
+        }
 
-AudioConnection          voiceMixing001(SharedAudio[0].voiceMixerM, 0, voiceMixer1, 0);
-AudioConnection          voiceMixing002(SharedAudio[1].voiceMixerM, 0, voiceMixer1, 1);
+        connections.push_back(new AudioConnection{voiceMixer[0], 0, voiceMixerM, 0});
+        connections.push_back(new AudioConnection{voiceMixer[1], 0, voiceMixerM, 1});
+        connections.push_back(new AudioConnection{voiceMixer[2], 0, voiceMixerM, 2});
+        connections.push_back(new AudioConnection{voiceMixerM, 0, dcOffsetFilter, 0});
+        connections.push_back(new AudioConnection{dcOffsetFilter, 2, volumeMixer, 0});
+        connections.push_back(new AudioConnection{volumeMixer, 0, ensemble, 0});
+        connections.push_back(new AudioConnection{dcOffsetFilter, 2, scope, 0});
+        connections.push_back(new AudioConnection{dcOffsetFilter, 2, peak, 0});
+        connections.push_back(new AudioConnection{ensemble, 0, effectMixerL, 1});
+        connections.push_back(new AudioConnection{ensemble, 1, effectMixerR, 1});
+        connections.push_back(new AudioConnection{volumeMixer, 0, effectMixerL, 0});
+        connections.push_back(new AudioConnection{volumeMixer, 0, effectMixerR, 0});
+        connections.push_back(new AudioConnection{effectMixerR, 0, usbAudio, 1});
+        connections.push_back(new AudioConnection{effectMixerR, 0, i2s, 1});
+        connections.push_back(new AudioConnection{effectMixerL, 0, i2s, 0});
+        connections.push_back(new AudioConnection{effectMixerL, 0, usbAudio, 0});
 
-AudioConnection          patchCord197(voiceMixer1, 0, voiceMixerM, 0);
-AudioConnection          patchCord198(voiceMixer2, 0, voiceMixerM, 1);
-AudioConnection          patchCord215(voiceMixer3, 0, voiceMixerM, 2);
-AudioConnection          patchCord203(voiceMixerM, 0, dcOffsetFilter, 0);
-AudioConnection          patchCord441(dcOffsetFilter, 2, volumeMixer, 0);
-AudioConnection          patchCord112(volumeMixer, 0, ensemble, 0);
-AudioConnection          patchCord415(dcOffsetFilter, 2, scope, 0);
-AudioConnection          patchCord416(dcOffsetFilter, 2, peak, 0);
-AudioConnection          patchCord113(ensemble, 0, effectMixerL, 1);
-AudioConnection          patchCord114(ensemble, 1, effectMixerR, 1);
-AudioConnection          patchCord115(volumeMixer, 0, effectMixerL, 0);
-AudioConnection          patchCord116(volumeMixer, 0, effectMixerR, 0);
-AudioConnection          patchCord117(effectMixerR, 0, usbAudio, 1);
-AudioConnection          patchCord118(effectMixerR, 0, i2s, 1);
-AudioConnection          patchCord119(effectMixerL, 0, i2s, 0);
-AudioConnection          patchCord120(effectMixerL, 0, usbAudio, 0);
-AudioControlSGTL5000     sgtl5000_1;     //xy=2353,505
-// GUItool: end automatically generated code
+        constant1Dc.amplitude(1.0);
 
+        voiceMixerM.gain(0, 0.25f);
+        voiceMixerM.gain(1, 0.25f);
+        voiceMixerM.gain(2, 0.25f);
+        voiceMixerM.gain(3, 0.25f);
 
-// Undefine custom classes renaming:
-#undef AudioSynthWaveform
-#undef AudioSynthWaveformModulated
+        pink.amplitude(1.0);
+        white.amplitude(1.0);
+
+        voiceMixerM.gain(0, mixerLevel);
+        voiceMixerM.gain(1, mixerLevel);
+        voiceMixerM.gain(2, mixerLevel);
+        voiceMixerM.gain(3, mixerLevel);
+
+        //This removes dc offset (mostly from unison pulse waves) before the ensemble effect
+        dcOffsetFilter.octaveControl(1.0f);
+        dcOffsetFilter.frequency(12.0f);//Lower values will give clicks on note on/off
+
+        volumeMixer.gain(0, 1.0f);
+        volumeMixer.gain(1, 0);
+        volumeMixer.gain(2, 0);
+        volumeMixer.gain(3, 0);
+    }
+};
 
 #endif
