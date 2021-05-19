c624a3c58676ad286e6ea5c38503bae92877c0fe
ElectroTechnique <50934180+ElectroTechnique@users.noreply.github.com>
2021-05-19 21:08:38 +0900

    Merge multi-timbral and exp glide

--- TSynth/Settings.h
+++ TSynth/Settings.h
@@ -55,7 +55,9 @@ FLASHMEM void settingsGlideShape(const char * value) {
   if (strcmp(value, "Lin") == 0) glideShape = 0;
   else if (strcmp(value, "Exp") == 0) glideShape = 1;
   else glideShape = 1;
-  VG_FOR_EACH_OSC(glide_.setMode(glideShape));
+  for (uint8_t i = 0; i < global.maxVoices(); i++) {
+    global.Oscillators[i].glide_.setMode(glideShape);
+  }
   storeGlideShape(glideShape); 
 }
 
@@ -89,7 +91,9 @@ FLASHMEM void settingsAmpEnv(const char * value) {
   else if (strcmp(value, "Exp +7") == 0)  envTypeAmp = 7;
   else if (strcmp(value, "Exp +8") == 0)  envTypeAmp = 8;
   else envTypeAmp = -128;
-  VG_FOR_EACH_OSC(ampEnvelope_.setEnvType(envTypeAmp));
+  for (uint8_t i = 0; i < global.maxVoices(); i++) {
+    global.Oscillators[i].ampEnvelope_.setEnvType(envTypeAmp);
+  }
   storeAmpEnv(envTypeAmp);
 }
 
@@ -113,23 +117,31 @@ FLASHMEM void settingsFiltEnv(const char * value) {
   else if (strcmp(value, "Exp +7") == 0)  envTypeFilt = 7;
   else if (strcmp(value, "Exp +8") == 0)  envTypeFilt = 8;
   else envTypeFilt = -128;
-  VG_FOR_EACH_OSC(filterEnvelope_.setEnvType(envTypeFilt));
+  for (uint8_t i = 0; i < global.maxVoices(); i++) {
+    global.Oscillators[i].filterEnvelope_.setEnvType(envTypeFilt);
+  }
   storeFiltEnv(envTypeFilt);
 }						
 
 FLASHMEM void reloadGlideShape(){
   glideShape = getGlideShape();
-  VG_FOR_EACH_OSC(glide_.setMode(glideShape));
+  for (uint8_t i = 0; i < global.maxVoices(); i++) {
+    global.Oscillators[i].glide_.setMode(glideShape);
+  }
 }
 
 FLASHMEM void reloadAmpEnv(){
   envTypeAmp = getAmpEnv();
-  VG_FOR_EACH_OSC(ampEnvelope_.setEnvType(envTypeFilt));
+  for (uint8_t i = 0; i < global.maxVoices(); i++) {
+    global.Oscillators[i].ampEnvelope_.setEnvType(envTypeAmp);
+  }
 }
 
 FLASHMEM void reloadFiltEnv(){
   envTypeFilt = getFiltEnv();
-  VG_FOR_EACH_OSC(filterEnvelope_.setEnvType(envTypeFilt));
+  for (uint8_t i = 0; i < global.maxVoices(); i++) {
+    global.Oscillators[i].filterEnvelope_.setEnvType(envTypeFilt);
+  }
 }
 
 FLASHMEM void settingsMIDICh(const char * value) {
@@ -219,7 +231,7 @@ FLASHMEM void settingsMonophonic(const char * value) {
   if (strcmp(value, "Highest") == 0) monophonic = MONOPHONIC_HIGHEST;
   if (strcmp(value, "Lowest") == 0)  monophonic = MONOPHONIC_LOWEST;
   if (strcmp(value, "Legato") == 0)  monophonic = MONOPHONIC_LEGATO;
-  voices.setMonophonic(monophonic);
+  groupvec[activeGroupIndex]->setMonophonic(monophonic);
 }
 
 FLASHMEM void settingsScopeEnable(const char * value) {
@@ -256,9 +268,9 @@ FLASHMEM int currentIndexVelocitySens() {
 }
 
 FLASHMEM int currentIndexKeyTracking() {
-  if (voices.getKeytrackingAmount() == 0.0f) return 0;
-  if (voices.getKeytrackingAmount() == 0.5f) return 1;
-  if (voices.getKeytrackingAmount() == 1.0f) return 2;
+  if (groupvec[activeGroupIndex]->getKeytrackingAmount() == 0.0f) return 0;
+  if (groupvec[activeGroupIndex]->getKeytrackingAmount() == 0.5f) return 1;
+  if (groupvec[activeGroupIndex]->getKeytrackingAmount() == 1.0f) return 2;
   return 0;
 }
 
@@ -295,7 +307,7 @@ FLASHMEM int currentIndexBassEnhanceEnable() {
 }
 
 FLASHMEM int currentIndexMonophonicMode() {
-  return voices.getMonophonicMode();
+  return groupvec[activeGroupIndex]->getMonophonicMode();
 }
 
 FLASHMEM int currentIndexScopeEnable() {
@@ -326,7 +338,7 @@ FLASHMEM void setUpSettings() {
   settingsOptions.push(SettingsOption{"Amp Env", {"Lin", "Exp -8", "Exp -7", "Exp -6", "Exp -5", "Exp -4", "Exp -3", "Exp -2", "Exp -1", "Exp 0", "Exp +1", "Exp +2", "Exp +3", "Exp +4", "Exp +5", "Exp +6", "Exp +7", "Exp +8", "\0"}, settingsAmpEnv, currentIndexAmpEnv});
   settingsOptions.push(SettingsOption{"Fil Env", {"Lin", "Exp -8", "Exp -7", "Exp -6", "Exp -5", "Exp -4", "Exp -3", "Exp -2", "Exp -1", "Exp 0", "Exp +1", "Exp +2", "Exp +3", "Exp +4", "Exp +5", "Exp +6", "Exp +7", "Exp +8", "\0"}, settingsFiltEnv, currentIndexFiltEnv});
   settingsOptions.push(SettingsOption{"Glide Shape", {"Lin", "Exp", "\0"}, settingsGlideShape, currentIndexGlideShape});
-  settingsOptions.push(SettingsOption{"Pick-up", {"Off", "On", "\0"}, settingsPickupEnable, currentIndexPickupEnable});																											
+  settingsOptions.push(SettingsOption{"Pick-up", {"Off", "On", "\0"}, settingsPickupEnable, currentIndexPickupEnable});
   settingsOptions.push(SettingsOption{"Encoder", {"Type 1", "Type 2", "\0"}, settingsEncoderDir, currentIndexEncoderDir});
   settingsOptions.push(SettingsOption{"Oscilloscope", {"Off", "On", "\0"}, settingsScopeEnable, currentIndexScopeEnable});
   settingsOptions.push(SettingsOption{"VU Meter", {"Off", "On", "\0"}, settingsVUEnable, currentIndexVUEnable});
